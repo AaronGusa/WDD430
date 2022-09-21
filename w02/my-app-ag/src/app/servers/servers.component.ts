58685b0ffcd3966b761be1fe17864905b81578f4
Goosa2025 <gus21002@byui.edu>
2022-09-21 13:37:15 -0600

    mods in tutorial

--- w02/my-app-ag/src/app/servers/servers.component.ts
+++ w02/my-app-ag/src/app/servers/servers.component.ts
@@ -2,14 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>`
+
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
 
-  constructor() { }
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'test Server';
+  
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created with the Server Name of ' + this.serverName;
+
+  }
+
+  onUpdateServerName(event: any) {
+    // console.log(event);
+    this.serverName = event.target.value;
+//    this.serverName = <HTMLInputElement>.event.target.value; //optional alternative to previous line
+
   }
 
 }
