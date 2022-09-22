c303017f3ef3cd59f944fa5c0485013364013c8e
Goosa2025 <gus21002@byui.edu>
2022-09-22 16:38:46 -0600

    Contact Components added; Header started

--- w02/cms/src/app/app.module.ts
+++ w02/cms/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ w02/cms/src/app/contacts/contact.model.ts
@@ -0,0 +1,3 @@
+export class Contact {
+    constructor(public id: number, public name: string, public email: any, public phone: number, public imageUrl: string, public group: any [] ) {}
+}
