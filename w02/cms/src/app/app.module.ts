f3851a75308f5bd26bfa043d2c968e07ee6a4ead
Goosa2025 <gus21002@byui.edu>
2022-09-22 12:30:36 -0600

    w02 mods

--- w02/cms/src/app/app.module.ts
+++ w02/cms/src/app/app.module.ts
@@ -6,6 +6,14 @@ import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ListsComponent } from './shopping-list/lists/lists.component';
+import { IngredientsComponent } from './shopping-list/ingredients/ingredients.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipe-book/recipe/recipe.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +21,15 @@ import { ContactDetailComponent } from './contacts/contact-detail/contact-detail
     HeaderComponent,
     ContactsComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    ShoppingListComponent,
+    ListsComponent,
+    IngredientsComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
