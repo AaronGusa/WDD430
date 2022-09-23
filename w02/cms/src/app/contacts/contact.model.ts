af781dc0850790c5ee1852beddaa5bc54007dc26
Goosa2025 <gus21002@byui.edu>
2022-09-23 09:38:07 -0600

    cms Contact Build

--- w02/cms/src/app/contacts/contact.model.ts
+++ w02/cms/src/app/contacts/contact.model.ts
@@ -1,3 +1,8 @@
 export class Contact {
-    constructor(public id: number, public name: string, public email: any, public phone: number, public imageUrl: string, public group: any [] ) {}
+    constructor(public id: number,
+    public name: string, 
+    public email: any, 
+    public phone: string, 
+    public imageUrl: string, 
+    public group: any [] ) {}
 }
