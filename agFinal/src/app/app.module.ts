7e50114043ef536973bcb68c76377e5a5682ab68
Goosa2025 <gus21002@byui.edu>
2022-12-06 20:24:27 -0700

    quick framing

--- agFinal/src/app/app.module.ts
+++ agFinal/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgMaterialModule } from './ng-material/ng-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PeopleComponent } from './people/people.component';
+import { ListsComponent } from './lists/lists.component';
+import { GiftsComponent } from './gifts/gifts.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PeopleComponent,
+    ListsComponent,
+    GiftsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
