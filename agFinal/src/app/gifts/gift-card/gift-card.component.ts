551ddcf7b81d6e242ccf31648a731c26236a8aad
Goosa2025 <gus21002@byui.edu>
2022-12-12 19:27:49 -0700

    Gifting work

--- agFinal/src/app/gifts/gift-card/gift-card.component.ts
+++ agFinal/src/app/gifts/gift-card/gift-card.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Gifts } from '../gift.model';
+import { GiftsService } from '../gifts.service';
 
 @Component({
   selector: 'app-gift-card',
   templateUrl: './gift-card.component.html',
   styleUrls: ['./gift-card.component.css']
 })
-export class GiftCardComponent {
+export class GiftCardComponent implements OnInit {
+  private giftListChanged: Subscription;
+  private bookListChanged: Subscription;
+  private clothingListChanged: Subscription;
+  private electronicsListChanged: Subscription;
+  private gamesListChanged: Subscription;
+  private toysListChanged: Subscription;
+  private miscListChanged: Subscription;
 
+
+  gifts: Gifts[] = [];
+  gift: Gifts;
+  bookList: Gifts[];
+  clothingList: Gifts[];
+  electronicsList: Gifts[];
+  gamesList: Gifts[];
+  toysList: Gifts[];
+  miscList: Gifts[];
+  
+  constructor(private gService: GiftsService,
+    private http: HttpClientModule) {
+
+    }
+
+  ngOnInit() {
+    this.gifts = this.gService.getGifts();
+    // console.log(this.clothingList);
+    // this.onCategorize();
+    // console.log(this.clothingList);
+    
+    this.giftListChanged = this.gService.giftsChangedEvent
+    .subscribe((gifts: Gifts[]) => {
+      this.gifts = gifts;
+      
+      //console.log(this.gifts);
+    });
+    
+    this.bookListChanged = this.gService.bookListSub
+    .subscribe((books: Gifts[]) => {
+      this.bookList = books;
+      //console.log(this.gifts);
+    });
+   this.clothingListChanged = this.gService.clothingListSub
+   .subscribe((clothing: Gifts[]) => {
+     this.clothingList = clothing;
+     //console.log(this.gifts);
+   });
+   this.electronicsListChanged = this.gService.electronicsListSub
+   .subscribe((electronics: Gifts[]) => {
+     this.electronicsList = electronics;
+     //console.log(this.gifts);
+   });
+   this.gamesListChanged = this.gService.gamesListSub
+   .subscribe((games: Gifts[]) => {
+     this.gamesList = games;
+     //console.log(this.gifts);
+   });
+   this.toysListChanged = this.gService.toysListSub
+   .subscribe((toys: Gifts[]) => {
+     this.toysList = toys;
+     //console.log(this.gifts);
+   });
+   this.miscListChanged = this.gService.miscListSub
+   .subscribe((misc: Gifts[]) => {
+     this.miscList = misc;
+     //console.log(this.gifts);
+   });
+
+   
+
+  }
+
+  // onCategorize() {
+  //   for (let gift of this.gifts) {
+  //     if (gift.category === 1) {
+  //       this.bookList.push(gift);
+  //     } else if (gift.category === 2) {
+  //       this.clothingList.push(gift);
+        
+  //     } else if (gift.category === 3) {
+  //       this.electronicsList.push(gift);
+        
+  //     } else if (gift.category === 4) {
+  //       this.gamesList.push(gift);
+
+  //     } else if (gift.category === 5) {
+  //       this.toysList.push(gift);
+  //     } else if (gift.category === 6) {
+  //       this.miscList.push(gift);
+  //     } 
+  //   }
+  // }
 }
--- agFinal/src/app/gifts/gifts.service.ts
+++ agFinal/src/app/gifts/gifts.service.ts
@@ -12,13 +12,35 @@ export class GiftsService implements OnInit {
   gifts: Gifts[] = [];
   giftsChangedEvent = new Subject<Gifts[]>();
   giftsTested: Gifts[];
+  category: Array<any> = [
+    {1: "Books"},
+    {2: "Clothing"},
+    {3: "Electronics"},
+    {4: "Games"},
+    {5: "Toys"},
+    {6: "Misc"}
+  ];
+  
+  
+  bookList: Gifts[] = [];
+  clothingList: Gifts[] = [];
+  electronicsList: Gifts[] = [];
+  gamesList: Gifts[] = [];
+  toysList: Gifts[] = [];
+  miscList: Gifts[] = [];
+
+  bookListSub = new Subject<Gifts[]>();
+  clothingListSub = new Subject<Gifts[]>();
+  electronicsListSub = new Subject<Gifts[]>();
+  gamesListSub = new Subject<Gifts[]>();
+  toysListSub = new Subject<Gifts[]>();
+  miscListSub = new Subject<Gifts[]>();
 
   constructor(private http: HttpClient,
               private route: ActivatedRoute,
               private router: Router) {
     this.fetchGifts();
-    this.giftsChangedEvent.next(this.gifts.slice());
-
+    
     //console.log('Gift OnConstruct');
     //console.log(this.gifts);
   }
@@ -33,7 +55,15 @@ export class GiftsService implements OnInit {
     //console.log('Gift OnInit');
     //console.log(this.gifts.length);
     //this.fetchGifts();
+    //this.giftsChangedEvent.next(this.gifts.slice());
     this.giftsChangedEvent.next(this.gifts.slice());
+    //this.onCategorize()
+    // this.bookListSub.next([...this.bookList]);
+    // this.clothingListSub.next([...this.clothingList]);
+    // this.electronicsListSub.next([...this.electronicsList]);
+    // this.gamesListSub.next([...this.gamesList]);
+    // this.toysListSub.next([...this.toysList]);
+    // this.miscListSub.next([...this.miscList]);
     // if (this.gifts.length === 0) {
     //   this.fetchGifts();
     //   this.giftsChangedEvent.next(this.gifts.slice());
@@ -45,41 +75,30 @@ export class GiftsService implements OnInit {
     // console.log(this.gifts);
   }
 
- fetchGifts() {
-    this.http
+ async fetchGifts() {
+    await this.http
     .get<{message:string, Gifts: Gifts[]}>('http://localhost:5000/gifts')
     .subscribe((response) => {
       this.gifts = response.Gifts;
       console.log(response.message);
       this.giftsChangedEvent.next(this.gifts.slice());
-      //console.log(this.gifts);
-    });
+      this.onCategorize();
+      }
+      
+    );
     
     //console.log(this.gifts);
   }
 
   getGifts() {
-    
     return this.gifts.slice();
   }
 
   getGift(giftNumber: number) {
-    if (this.gifts.length === 0) {
-      this.fetchGifts();
       for (let gift of this.gifts) {
         if (gift.giftNumber === giftNumber) {
-            this.giftsChangedEvent.next(this.gifts.slice());
           return gift;
         }}
-    } else {
-      for (let gift of this.gifts) {
-        if (gift.giftNumber === giftNumber) {
-          return gift;
-        } else {
-            console.log('NO');
-        }
-      }
-    }
     this.giftsChangedEvent.next(this.gifts.slice());
   }
 
@@ -87,5 +106,38 @@ export class GiftsService implements OnInit {
     console.log(this.gifts.slice());
   }
 
+  onCategorize() {
+    
+    if(this.gifts.length > 0) {
+      for (let gift of this.gifts) {
+        if (gift.category === 1) {
+          this.bookList.push(gift);
+        } else if (gift.category === 2) {
+          this.clothingList.push(gift);
+          
+        } else if (gift.category === 3) {
+          this.electronicsList.push(gift);
+          
+        } else if (gift.category === 4) {
+          this.gamesList.push(gift);
+
+        } else if (gift.category === 5) {
+          this.toysList.push(gift);
+        } else if (gift.category === 6) {
+          this.miscList.push(gift);
+      } else {
+        this.onCategorize();
+        console.log('loading...')
+      }
+    }
+  }
+    this.giftsChangedEvent.next(this.gifts.slice());
+    this.bookListSub.next([...this.bookList]);
+    this.clothingListSub.next([...this.clothingList]);
+    this.electronicsListSub.next([...this.electronicsList]);
+    this.gamesListSub.next([...this.gamesList]);
+    this.toysListSub.next([...this.toysList]);
+    this.miscListSub.next([...this.miscList]);
+   }
 
 }
