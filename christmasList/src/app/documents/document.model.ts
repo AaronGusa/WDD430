f54f0a9efcda23fce8e93075fc4a3a30f51534cf
Goosa2025 <gus21002@byui.edu>
2022-12-13 10:18:39 -0700

    Removed Repo

--- christmasList/src/app/documents/document.model.ts
+++
@@ -1,9 +0,0 @@
-export class Document {
-    constructor(
-        public id: string,
-        public name: string,
-        public description: any,
-        public url: any,
-        public children: {}
-    ) {}
-}
--- christmasList/src/app/messages/message.model.ts
+++
@@ -1,8 +0,0 @@
-export class Messages {
-    constructor(
-        public id: string,
-        public subject: any,
-        public msgText: any,
-        public sender: string
-    ) {}
-}
